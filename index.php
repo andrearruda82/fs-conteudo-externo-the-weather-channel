92aca4031db50ef4405d52d16b37d50255c78666
André Arruda <andrearruda82@gmail.com>
2016-10-10 10:16:48 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+setlocale(LC_ALL, "pt_BR", "pt_BR.iso-8859-1", "pt_BR.utf-8", "portuguese");
+date_default_timezone_set('America/Sao_Paulo');
+
+// To help the built-in PHP dev server, check if the request was actually for
+// something which should probably be served as a static file
+if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
+    return false;
+}
+
+require __DIR__ . '/vendor/autoload.php';
+
+session_start();
+
+// Instantiate the app
+$settings = require __DIR__ . '/app/settings.php';
+$app = new \Slim\App($settings);
+
+// Set up dependencies
+require __DIR__ . '/app/dependencies.php';
+
+// Register middleware
+require __DIR__ . '/app/middleware.php';
+
+// Register routes
+require __DIR__ . '/app/routes.php';
+
+// Run!
+$app->run();
